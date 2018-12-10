d05b84baadd74b6273cedb8bd5e609994abdb847
An <an.arletta.cattie@gmail.com>
2018-12-10 11:55:50 +0100

    no message

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-//oefening 1
-//let variabele = "Anneke"
-//console.log(variabele);
+// oefening 1
+let variabele = "Anneke"
+console.log(variabele);
 
-//oefening 2
-//let name = "Doe"
-//let firstname = "John"
-//let city = "New-York"
-// alert("Name:"+" "+name+" "+"\nFirstname:"+" "+firstname+" "+"\nCity:"+" "+city);
+// oefening 2
+let name = "Doe"
+let firstname = "John"
+let city = "New-York"
+ alert("Name:"+" "+name+" "+"\nFirstname:"+" "+firstname+" "+"\nCity:"+" "+city);
 
-//oefening 3
-//let person = prompt("Please enter your firstname", "An");
-//    alert("Hallo " + person);
+// oefening 3
+let person = prompt("Please enter your firstname", "An");
+    alert("Hallo " + person);
 
-//oefening 4
-//let voornaam = prompt("Wat is uw naam?", "An")
-//let achternaam = prompt("Wat is uw achternaam?","Cattie")
-//let plaats = prompt("Wat is uw gemeente?","Genk")
-//alert("uw naam is "+achternaam+" "+voornaam+" en u woont in "+plaats)
+// oefening 4
+let voornaam = prompt("Wat is uw naam?", "An")
+let achternaam = prompt("Wat is uw achternaam?","Cattie")
+let plaats = prompt("Wat is uw gemeente?","Genk")
+alert("uw naam is "+achternaam+" "+voornaam+" en u woont in "+plaats)
 
-//oefening 5
-//resultaat=""
-//let eerstecijfer = prompt("geef een cijfer",4);
-//let tweedecijfer = prompt("geef nog een cijfer",7);
-//resultaat = (eerstecijfer * tweedecijfer);
-//alert(resultaat)
+// oefening 5
+resultaat=""
+let eerstecijfer = prompt("geef een cijfer",4);
+let tweedecijfer = prompt("geef nog een cijfer",7);
+resultaat = (eerstecijfer * tweedecijfer);
+alert(resultaat)
 
-//Oefening 6
-//function restDeling() {
-//  let firstnummer = document.getElementById("firstNumber").value;
-//  let lastnummer = document.getElementById("secondNumber").value;
-//  alert(firstnummer%lastnummer);
-//}
+// oefening 6
+function restDeling() {
+  let firstnummer = document.getElementById("firstNumber").value;
+  let lastnummer = document.getElementById("secondNumber").value;
+  alert(firstnummer%lastnummer);
+}
 
-//oefening 7
+// oefening 7
 function berekening(){
   let lengte = document.getElementById("height").value;
   let geboortejaar = document.getElementById("birthdate").value;
@@ -44,7 +44,7 @@ function berekening(){
   alert(lengte);
 }
 
-//oefening 8
+// oefening 8
 function volwassen(){
   let leeftijd = document.getElementById("age").value;
   if (leeftijd >= 18) {
